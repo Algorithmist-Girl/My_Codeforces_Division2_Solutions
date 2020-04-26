94d2431beac1140283ec1a4141481a9ecc534d45
Deepansha <36552319+Algorithmist-Girl@users.noreply.github.com>
2020-04-26 16:19:38 -0400

    Add files via upload

--- exercisingWalk.java
+++ exercisingWalk.java
@@ -7,8 +7,10 @@ public class exercisingWalk {
     private static int x, y, x1, x2, y1, y2;
 
     public static boolean compute() {
-        if (x - a + b >= x1 && x - a + b <= x2)
-            if (y - c + d >= y1 && y - c + d <= y2)
+
+        // OR STATEMENT AT END COVERS THE 3RD CASE IN EXAMPLE!!!
+        if (x - a + b >= x1 && x - a + b <= x2 && ((x1 < x2) || (a + b == 0))) // IF X1=X2, A+B MUST BE = 0
+            if (y - c + d >= y1 && y - c + d <= y2 && ((y1 < y2) || (c + d == 0)))
                 return true;
         return false;
     }
@@ -17,6 +19,8 @@ public class exercisingWalk {
         Scanner sc = new Scanner(System.in);
 
         PrintWriter out = new PrintWriter(System.out);
+
+        t = sc.nextInt();
 
         for (int i = 0; i < t; i++) {
             sc.nextLine();
@@ -29,12 +33,12 @@ public class exercisingWalk {
             x = sc.nextInt();
             y = sc.nextInt();
             x1 = sc.nextInt();
-            x2 = sc.nextInt();
             y1 = sc.nextInt();
+            x2 = sc.nextInt();
             y2 = sc.nextInt();
 
             if (compute())
-                out.print("Yes");
+                out.println("Yes");
             else
                 out.println("No");
         }
